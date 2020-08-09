16f91d8ae08b03708b44018d60f30d20d3b6a726
Ashwani <ashwaniagarwal226@gmail.com>
2020-08-09 20:59:17 +0530

    @Repository was not working issue fix

--- src/main/java/org/example/mongo/SpringBootMongoDbApplication.java
+++ src/main/java/org/example/mongo/SpringBootMongoDbApplication.java
@@ -1,9 +1,15 @@
 package org.example.mongo;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableAutoConfiguration
+@ComponentScan("org.example.mongo.repos")
 public class SpringBootMongoDbApplication {
 
     public static void main(String... args) {
