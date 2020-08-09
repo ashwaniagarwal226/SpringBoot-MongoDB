ef72f539275dc8bd5a8804133a24c2707b58d4b8
Ashwani <ashwaniagarwal226@gmail.com>
2020-08-09 15:27:01 +0530

    initial commit

---
+++ src/main/java/org/example/mongo/SpringBootMongoDbApplication.java
@@ -0,0 +1,11 @@
+package org.example.mongo;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMongoDbApplication {
+
+    public static void main(String... args) {
+
+    }
+}
