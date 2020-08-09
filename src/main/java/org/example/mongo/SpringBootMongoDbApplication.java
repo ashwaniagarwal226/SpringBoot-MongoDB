843f4c4839face24f9c279318ba7bb7b48b2301f
Ashwani <ashwaniagarwal226@gmail.com>
2020-08-09 21:09:26 +0530

    import channge

--- src/main/java/org/example/mongo/SpringBootMongoDbApplication.java
+++ src/main/java/org/example/mongo/SpringBootMongoDbApplication.java
@@ -4,8 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
