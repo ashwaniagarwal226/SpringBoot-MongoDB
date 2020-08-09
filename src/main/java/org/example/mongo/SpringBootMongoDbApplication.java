93815a6b27de27fd5d1d23009c5cd24c7f0f8d19
Ashwani <ashwaniagarwal226@gmail.com>
2020-08-09 21:08:48 +0530

    Swagger issue fix

--- src/main/java/org/example/mongo/SpringBootMongoDbApplication.java
+++ src/main/java/org/example/mongo/SpringBootMongoDbApplication.java
@@ -6,10 +6,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableAutoConfiguration
 @ComponentScan("org.example.mongo.repos")
+@EnableSwagger2
 public class SpringBootMongoDbApplication {
 
     public static void main(String... args) {
